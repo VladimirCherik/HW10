f66049e82be97069d3aa79e1ea341d6fd94a8cbe
VladimirCherik <vladimir_cherik@i.ua>
2023-09-12 14:29:32 +0300

    Added regex in first task

--- src/main/java/org/example/HW10_1.java
+++ src/main/java/org/example/HW10_1.java
@@ -1,11 +1,16 @@
 package org.example;
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HW10_1 {
     private static final String ABSOLUTE_PATH = "C:\\Users\\Admin\\IdeaProjects\\HW10\\src\\main\\resources\\HW10_1\\firstFile.txt";
+    private static final String rightTypeNumber = "((\\(\\d{3}\\))\\s|(\\d{3}\\-))(\\d{3}\\-\\d{4})";
 
     public static void main(String[] args)  {
+
+
 
         File file = new File(ABSOLUTE_PATH);
         ifFileExist(file);
@@ -20,9 +25,14 @@ public class HW10_1 {
         }
 
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
+
+            Pattern pt1 = Pattern.compile(rightTypeNumber);
             String line = bufferedReader.readLine();
+
+
             while (line != null){
-                if(line.contains(")") && line.contains("(") || line.contains("-")){
+                Matcher mt = pt1.matcher(line);
+                while (mt.find()){
                     System.out.println(line);
                 }
                 line = bufferedReader.readLine();
--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,6 +3,9 @@ package org.example;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -13,5 +16,18 @@ public class Test {
         System.out.println(json);
 
         System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(person));
+
+
+
+
+        String s = "the day is sunny 22 the the\n" +
+                "the sunny is 98 is";
+        Pattern pt = Pattern.compile("\\d\\d");
+        Matcher mt = pt.matcher(s);
+        String group = "";
+        while (mt.find()){
+            group += mt.group() + " ";
+        }
+        System.out.println(group);
     }
 }
